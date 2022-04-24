b303ceb693d3ca290556c17896ed9e825aeff01e
arthur <arthur.damous@gmail.com>
2022-04-24 03:41:14 -0300

    koltin kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,18 @@
 buildscript {
-    ext {
-        compose_version = '1.0.1'
-    }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.4"
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20'
+        classpath(Build.androidBuildTools)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.kotlinGradlePlugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class){
+    delete(rootProject.buildDir)
 }
