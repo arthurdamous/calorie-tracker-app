18a18635fd84251660a4d645d68129a4e9adc5f8
Arthur Damous <arthur@MacBook-Pro-de-Arthur.local>
2022-06-07 01:23:23 -0300

    Finished the course

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         versionCode = ProjectConfig.versionCode
         versionName = ProjectConfig.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "br.damous.calorietrackerapp.HiltTestRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
---
+++ app/src/main/java/br/damous/calorietrackerapp/util/NavControllerExt.kt
@@ -0,0 +1,5 @@
+package br.damous.calorietrackerapp.util
+
+import androidx.navigation.NavController
+import br.damous.core.util.UiEvent
+
